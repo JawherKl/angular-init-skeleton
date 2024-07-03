3feb20127afccc941363519c67e22a1e95f24e17
JawherKl <kalleljawher4@gmail.com>
2024-07-03 16:50:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-init-skeleton
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/JawherKl/angular-init-skeleton)
---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
